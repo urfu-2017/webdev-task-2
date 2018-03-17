930063b9b8df74ca95e4ef4e68be0235a5c3d4ce
Damir Shaikhislamov <jeminay.dev@gmail.com>
2018-03-17 15:11:33 +0500

    changes

--- controllers/location/all.js
+++ controllers/location/all.js
@@ -17,14 +17,16 @@ const transform = (object) => {
 
 export default (req, res) => {
     const { select, query, sortBy, reverse } = req.query
-    const { limit = Number.MAX_SAFE_INTEGER, offset = 0 } = req.query
-    const [lim, off] = [limit, offset].map(Number)
+    const [limit, offset] = [
+        req.query.limit || Number.MAX_SAFE_INTEGER,
+        req.query.offset || 0
+    ].map(Number)
 
     res.json(transform(DB.objects(Location.NAME))
         .when(sortBy)
         .then(self => self.sorted(sortBy))
-        .when(lim > 0 && off >= 0)
-        .then(self => self.slice(off, off + lim))
+        .when(limit > 0 && offset >= 0)
+        .then(self => self.slice(offset, offset + limit))
         .when(select)
         .then(self => self.filter(x => x[select] && (query
             ? String(x[select])
