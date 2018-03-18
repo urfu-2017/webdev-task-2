a1eabcc1a2fd4cd5025a5f30e5d2f2434585df54
DorokhovaTatiana <t.dorohovav@gmail.com>
2018-03-18 21:40:57 +0500

    add morgan

--- index.js
+++ index.js
@@ -2,8 +2,10 @@
 
 const express = require('express');
 const router = require('./router');
+const morgan = require('morgan');
 const app = express();
 
+app.use(morgan('dev'));
 app.use('/', router);
 app.listen('8080', () => {
     console.info('http://localhost:8080');
