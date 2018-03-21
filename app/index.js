c9139d17a6d9f6367eb607843a5eb1dbf6ba8068
lms <lms@skbkontur.ru>
2018-03-21 20:51:24 +0500

    add CORS

--- app/index.js
+++ app/index.js
@@ -13,6 +13,20 @@ for (let place of places) {
 
 const app = express();
 app.use(bodyParser.json());
+
+app.use((req, res, next) => {
+    res.header('Content-Type', 'application/json');
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PATCH,PUT,POST,DELETE,OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Content-Length, X-Requested-With');
+    res.header('Access-Control-Allow-Credentials', 'true');
+
+    if (req.method === 'OPTIONS') {
+        res.status(200).send();
+    } else {
+        next();
+    }
+});
 routes(app);
 
 let port = process.env.PORT || 8080;
