71ccf32c29947e64757c237d6c97bf655b8aa0f4
Admirallll <stanislav.yarkeev@gmail.com>
2018-03-22 17:37:21 +0500

    add now support + cors

--- index.js
+++ index.js
@@ -9,6 +9,13 @@ const placesContoller = require('./controllers/places');
 
 app.use(bodyParser.json());
 
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header("Access-Control-Allow-Methods", "POST, GET, DELETE, PATCH");
+    next();
+});
+
 app.post('/places', placesContoller.addPlace);
 app.get('/places', placesContoller.getPlaces);
 app.delete('/places', placesContoller.deletePlaces);
