edfd3561b2dfb78ad6aba2a1fd3774ae35bac719
Àëåêñàíäð ×åðíûøåâ <alekscher@apri-code.ru>
2018-03-22 19:26:55 +0500

    add CROS headers

--- index.js
+++ index.js
@@ -6,9 +6,16 @@ const express = require('express');
 
 const app = express();
 
+app.use(({req, res, next}) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
+    next();
+});
+
 routes(app);
 
-app.listen(8080, () => {
+app.listen(8081, () => {
     console.info('Open http://localhost:8080/');
 });
 
