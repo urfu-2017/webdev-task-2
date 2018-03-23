38d40ce0a0ed16f76d3f178f6ebee95d01256d7e
Àëåêñàíäð ×åðíûøåâ <alekscher@apri-code.ru>
2018-03-23 09:37:07 +0500

    fix style

--- index.js
+++ index.js
@@ -6,10 +6,10 @@ const express = require('express');
 
 const app = express();
 
-app.use(({req, res, next}) => {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
+app.use(({ req, res, next }) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
     next();
 });
 
